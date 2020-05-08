b43e24faf2a9340745070b4b471facb5882c94dc
Ahmed G. 3tiwy <63562536+3tiwy@users.noreply.github.com>
2020-05-08 11:51:14 +0200

    Create ipgeo.py

---
+++ ipgeo.py
@@ -0,0 +1,35 @@
+#python
+#simple ip Geolocation tool using api
+#3tiwy
+import requests
+class c:
+    HE = '\033[6m'
+    A = '\033[1m'
+    C = '\033[63m'
+    OKGREEN = '\033[4m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    E = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(c.WARNING+"""
+    ##########################################
+    ##      IP Geolocation                  ##
+    ##                               v1.0   ##
+    ## by Ahmed 3tiwy                       ##
+    ##########################################
+"""
+    +c.E)
+p=input("Enter the IP >" )
+url=("https://api.ipgeolocation.io/ipgeo?apiKey=2804d60b250245ce918b4e38085abe66&ip=")
+r = requests.get(url+p)
+x=r.json()
+print(c.A+"ip > "+c.E+x['ip'])
+print(c.A+"continent > "+c.E+x['continent_name'])
+print(c.A+"country > "+c.E+x['country_name'])
+print(c.A+"city > "+c.E+x['city'])
+print(c.A+"latitude > "+c.E+x['latitude'])
+print(c.A+"longitude > "+c.E+x['longitude'])
+print(c.A+"isp > "+c.E+x['isp'])
+print(c.A+"connection type > "+c.E+x['connection_type'])
